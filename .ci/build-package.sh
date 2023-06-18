2d9bba03106f2243a9a31e00558b9a7d622c14ce
Yoann Potinet <yoann.potinet@frozenstorminteractive.com>
2023-06-18 10:18:37 -0400

    Fix workflow

--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -7,6 +7,10 @@ if ! [[ $SPK_PACKAGE_BUILD_NUMBER =~ "^[0-9]+$" ]]; then
    printf -v SPK_PACKAGE_BUILD_NUMBER "%04d" "$SPK_PACKAGE_BUILD_NUMBER"
 fi
 
+if [ "$SPK_BETA" = true ] ; then
+    SPK_PACKAGE_BUILD_NUMBER+="b"
+fi
+
 SOURCE_DIRNAME=helixcoreserverpackage
 # move build files into source dir
 mkdir -p /toolkit/source
