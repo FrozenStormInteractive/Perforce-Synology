fc92c23c85327d5ba57486b4f4c49cce3ee113c9
Yoann Potinet <yoann.potinet@frozenstorminteractive.com>
2023-06-18 20:03:59 -0400

    Remove b suffix in beta versions

--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -7,10 +7,6 @@ if ! [[ $SPK_PACKAGE_BUILD_NUMBER =~ "^[0-9]+$" ]]; then
    printf -v SPK_PACKAGE_BUILD_NUMBER "%04d" "$SPK_PACKAGE_BUILD_NUMBER"
 fi
 
-if [ "$SPK_BETA" = true ] ; then
-    SPK_PACKAGE_BUILD_NUMBER+="b"
-fi
-
 if [ ! -z "$P4D_VERSION" ]; then
     SPK_PACKAGE_VERSION="${P4D_VERSION}-${SPK_PACKAGE_BUILD_NUMBER:-0001}"
 fi
